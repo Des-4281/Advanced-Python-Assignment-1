24da03f05b61b5541691e43e164af115706b792d
David Solorio <delosolorio@gmail.com>
2026-04-13 16:27:35 -0400

    added add grade and average grade function and updated init function

--- assignment-1.py
+++ assignment-1.py
@@ -1,5 +1,16 @@
 class Student:
-    def __init__(self):
+    def __init__(self, name, age, grade):
         self.name = name 
         self.age = age
         self.grade = grade
+
+    def add_grade(self, grade):
+        # Add a grade to the student's record
+        self.grade.append(grade)
+
+    def average_grade(self):
+        # Return the average of all grades
+        if len(self.grade) == 0:
+            return 0
+        return sum(self.grade) / len(self.grade)
+
