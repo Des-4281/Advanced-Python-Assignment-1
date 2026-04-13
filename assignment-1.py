70dcb52f21dd1ec11985a7fb18c45c7bbe9336e5
David Solorio <delosolorio@gmail.com>
2026-04-13 16:32:59 -0400

    added display and grades tuple functions and updated email values throughout

--- assignment-1.py
+++ assignment-1.py
@@ -1,7 +1,7 @@
 class Student:
-    def __init__(self, name, age, grade):
-        self.name = name 
-        self.age = age
+    def __init__(self, name, grade, email):
+        self.name = name
+        self.email = email
         self.grade = grade
 
     def add_grade(self, grade):
@@ -14,3 +14,16 @@ class Student:
             return 0
         return sum(self.grade) / len(self.grade)
 
+    def display_info(self):
+        # Display the student's information
+        print(f"Name: {self.name}")
+        print(f"Email: {self.email}")
+        print(f"Grades: {self.grade}")
+
+    def grades_tuple(self):
+        # Return the grades as a tuple
+        return tuple(self.grade)
+    
+    
+
+    
